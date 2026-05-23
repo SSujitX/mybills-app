6fe62ff25f48cd018cd83425704b36d53abb88a9
SSujitX <ssujitxx@gmail.com>
2026-05-24 01:51:27 +0600

    feat: customize MainActivity to set status bar color and appearance

--- android/app/src/main/java/com/mybills/app/MainActivity.java
+++ android/app/src/main/java/com/mybills/app/MainActivity.java
@@ -1,5 +1,24 @@
 package com.mybills.app;
 
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.Window;
+import androidx.core.view.WindowCompat;
+import androidx.core.view.WindowInsetsControllerCompat;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Window window = getWindow();
+        WindowCompat.setDecorFitsSystemWindows(window, true);
+        window.setStatusBarColor(Color.parseColor("#07070a"));
+
+        WindowInsetsControllerCompat controller = WindowCompat.getInsetsController(window, window.getDecorView());
+        if (controller != null) {
+            controller.setAppearanceLightStatusBars(false);
+        }
+    }
+}
